02baef4fcda6a2be124aede6000f54cba0b69b24
Aryan Rath <143625019+aryanrath123@users.noreply.github.com>
2024-09-10 15:00:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     function openPaymentPage() {
         var options = {
             key: "rzp_test_EwlhHS5sAjsq9S", // Replace with your Razorpay API key
-            amount: 5000 * 100, // Amount in paise
+            amount: 199 * 100, // Amount in paise
             currency: "INR",
             name: "BiJli Bachao",
             description: "Test Transaction",
